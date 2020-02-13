6057c58be4eec3ff677137c79e4f4d2f37f09029
thiago.brandt <thiago.brandt@somosiris.com>
2020-02-13 15:00:13 -0300

    initial commit

---
+++ Static/IrisSDKStatic.framework/Headers/IrisSDKStatic.h
@@ -0,0 +1,17 @@
+//
+//  IrisSDKStatic.h
+//  IrisSDKStatic
+//
+//  Created by Thiago Rodrigo Da Costa Brandt on 13/02/20.
+//  Copyright © 2020 Iris Data Driven Marketing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IrisSDKStatic.
+FOUNDATION_EXPORT double IrisSDKStaticVersionNumber;
+
+//! Project version string for IrisSDKStatic.
+FOUNDATION_EXPORT const unsigned char IrisSDKStaticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IrisSDKStatic/PublicHeader.h>
